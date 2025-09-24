5f0104609a82caa975b9df493511f9e7c8637828
jittrapol <jittrapol.i@psu.ac.th>
2025-09-24 14:09:37 +0700

    Move the object

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Light sceneLight;
+    public GameObject objectToMove;
     public void SaveSettings()
     {
         Debug.Log("Settings have been saved.");
         int isOn = sceneLight.enabled ? 1 : 0;
         PlayerPrefs.SetInt("IsLightOn", isOn);
+        PlayerPrefs.SetFloat("ObjectPosX", objectToMove.transform.position.x);
+        PlayerPrefs.SetFloat("ObjectPosY", objectToMove.transform.position.y);
+        PlayerPrefs.SetFloat("ObjectPosZ", objectToMove.transform.position.z);
     }
 
     public void LoadSettings()
@@ -23,5 +27,10 @@ public class GameManager : MonoBehaviour
             Debug.Log("Light is OFF");
             sceneLight.enabled = false;
         }
+
+        float x = PlayerPrefs.GetFloat("ObjectPosX");
+        float y = PlayerPrefs.GetFloat("ObjectPosY");
+        float z = PlayerPrefs.GetFloat("ObjectPosZ");
+        objectToMove.transform.position = new Vector3(x, y, z);
     }
 }
