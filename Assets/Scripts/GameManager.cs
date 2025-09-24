533ac345cb80186a74c6c06a26d2c3a1c096618b
jittrapol <jittrapol.i@psu.ac.th>
2025-09-24 13:59:40 +0700

    Mock up script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,20 @@ public class GameManager : MonoBehaviour
     public void SaveSettings()
     {
         Debug.Log("Settings have been saved.");
+
+        PlayerPrefs.SetInt("IsLightOn", 1);
     }
 
     public void LoadSettings()
     {
         Debug.Log("Settings have been loaded.");
+        if (PlayerPrefs.GetInt("IsLightOn") == 1)
+        {
+            Debug.Log("Light is ON");
+        }
+        else
+        {
+            Debug.Log("Light is OFF");
+        }
     }
 }
