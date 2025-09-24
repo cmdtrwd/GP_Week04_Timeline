05459a6bc209186519f9ec00a3c04166d82b3064
jittrapol <jittrapol.i@psu.ac.th>
2025-09-24 09:21:06 +0700

    Added Dialogue System Scripts

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text mainText;
+    public Animator anim;
+    private Queue<string> sentences;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogues _dialogue)
+    {
+        //Debug.Log("Starting conversation with " + _dialogue.name);
+
+        anim.SetBool("IsOpen", true);
+
+        nameText.text = _dialogue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in _dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            anim.SetBool("IsOpen", false);
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        mainText.text = sentence;
+
+        //Debug.Log(sentence);
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of Conversation");
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogues dialogue;
+
+    private void Start()
+    {
+        //TriggerDialouge();
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);        
+    }
+
+
+}
