2639efe1fa6c1148f9873d8941c2731b51d7321a
jittrapol <jittrapol.i@psu.ac.th>
2025-09-24 13:57:54 +0700

    Created a GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void SaveSettings()
+    {
+        Debug.Log("Settings have been saved.");
+    }
+
+    public void LoadSettings()
+    {
+        Debug.Log("Settings have been loaded.");
+    }
+}
